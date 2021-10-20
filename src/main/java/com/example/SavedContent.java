f3e57a234ca9f1d6f45146808afef7c4eed4b149
Shane Farmer <shane.farmer@gmail.com>
2021-10-20 19:33:48 +1100

    chore: try with a POJO rather than a record for the mapped entity

--- src/main/java/com/example/SavedContent.java
+++ src/main/java/com/example/SavedContent.java
@@ -10,19 +10,91 @@ import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.data.annotation.Where;
 import java.time.Instant;
+import java.util.Objects;
 
 @MappedEntity
 @Where("@.deleted is null")
-public record SavedContent(
-    @Id @GeneratedValue @Nullable Integer id,
-    @NonNull String uid,
-    @Nullable @DateCreated Instant created,
-    @Nullable @DateUpdated Instant updated,
-    @Nullable Instant deleted
-) {
+public class SavedContent {
 
-  public SavedContent(String uid) {
-    this(null, uid, null, null, null);
+  @Id
+  @GeneratedValue
+  @Nullable
+  private Integer id;
+
+  @NonNull private String uid;
+
+  @Nullable
+  @DateCreated
+  private Instant created;
+
+  @Nullable
+  @DateUpdated
+  private Instant updated;
+
+  @Nullable private Instant deleted;
+
+  @Nullable
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(@Nullable Integer id) {
+    this.id = id;
+  }
+
+  @NonNull
+  public String getUid() {
+    return uid;
+  }
+
+  public void setUid(@NonNull String uid) {
+    this.uid = uid;
+  }
+
+  @Nullable
+  public Instant getCreated() {
+    return created;
+  }
+
+  public void setCreated(@Nullable Instant created) {
+    this.created = created;
+  }
+
+  @Nullable
+  public Instant getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(@Nullable Instant updated) {
+    this.updated = updated;
+  }
+
+  @Nullable
+  public Instant getDeleted() {
+    return deleted;
+  }
+
+  public void setDeleted(@Nullable Instant deleted) {
+    this.deleted = deleted;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof SavedContent)) {
+      return false;
+    }
+    SavedContent that = (SavedContent) o;
+    return Objects.equals(getId(), that.getId()) && getUid().equals(that.getUid())
+        && Objects.equals(getCreated(), that.getCreated()) && Objects.equals(getUpdated(),
+        that.getUpdated()) && Objects.equals(getDeleted(), that.getDeleted());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getUid(), getCreated(), getUpdated(), getDeleted());
   }
 }
 
